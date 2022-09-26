a6193b42cdbba89037c37dc54fd3c0b4bf5fc672
Pavel <jrmarty@mail.ru>
2022-09-26 11:54:13 +0300

    films

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 "use strict";
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", "");
+const b = prompt("На сколько оцените его?", "");
+const c = prompt("Один из последних просмотренных фильмов?", "");
+const d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
