ca895c3716344d7c61ba6daf052e4ea9e3908719
Pavel <jrmarty@mail.ru>
2022-09-30 01:07:11 +0300

    update 29.09

--- project.js
+++ project.js
@@ -21,7 +21,7 @@ const personalMovieDB = {
 
 function rememberMyFilms() {
     for (let i = 0; i < 2; i++) {
-        const a = prompt("Один из последних просмотренных фильмов?", "");
+        const a = prompt("Один из последних просмотренных фильмов?", "").trim();
         const b = prompt("На сколько оцените его?", "");
         
         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
