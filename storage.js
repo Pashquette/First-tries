665ab0c8cb66c26a5a95003cac24c077907579ab
Pavel <jrmarty@mail.ru>
2022-10-01 01:15:30 +0300

    update 30.09

--- storage.js
+++ storage.js
@@ -255,3 +255,42 @@ function learnJS(lang, callback) {
     learnJS("JavaScript", function() {
         console.log("Я прошел этот урок!")
     });
+
+    // -----------------------------------------------------
+
+
+    const options = {
+        name: "test",
+        width: 1024,
+        height: 1024,
+        colors: {
+            border: "black",
+            bg: "Red",
+    
+        },
+        makeTest: function() {
+            console.log("test");
+        }
+    };
+    options.makeTest();
+    
+    const {border, bg} = options.colors;
+    console.log(border);
+    
+    console.log(Object.keys(options).length);
+    
+    console.log(options["colors"]["border"]);
+    let counter = 0;
+    for (let key in options) {
+        if (typeof(options[key]) === 'object') {
+            for (let i in options[key]) {
+                console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+            }
+        } else {
+            console.log(`Свойство ${key} имеет значение ${options[key]}`);
+            counter++;
+        }
+        
+    }
+    
+    console.log(counter);
