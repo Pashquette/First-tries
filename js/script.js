051d5f6865f94fd44ab4f83d9e7fd2a87c3e3eb4
Pavel <jrmarty@mail.ru>
2023-01-23 16:12:27 +0300

    23.01.23

--- js/script.js
+++ js/script.js
@@ -1,49 +1,101 @@
 'use strict';
 
-// console.log('Запрос данных...');
+// filter
 
-// const req = new Promise(function(resolve, reject) {
-//     setTimeout(() => {
-//         console.log("Подготовка данных...");
-    
-//         const product = {
-//             name: 'TV',
-//             price: 2000
-//         };
-    
-//         resolve(product);
-//     }, 2000);
-    
+// const names = ['Ivan', 'Ann', 'Ksenia', 'Voldemort'];
+
+// const shortNames = names.filter(function(name) {
+//     return name.length < 5;
 // });
 
-// req.then((product) => {
-//    return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             product.status = 'order';
-//             resolve(product);
-//         }, 2000);
-//     });
+// console.log(shortNames);
 
-// }).then(data => {
-//     data.modify = true;
-//     return data;
-// }).then((data) => {
-//     console.log(data);
-// }).catch(() => {
-//     console.error('Произошла ошибка');
-// }).finally(() => {
-//     console.log('Finally');
+// let answers = ['IvAn', 'AnnA', 'Hello'];
+
+// answers = answers.map(item => item.toLowerCase());
+
+// console.log(answers);
+
+// const some = ['4', '5', '543'];
+
+// console.log(some.some(item => typeof(item) === 'number'));
+
+// console.log(some.every(item => typeof(item) === 'string'));
+
+// let result = 0;
+
+// arr.forEach((item) => {
+//    result += item;
 // });
+// console.log(result);
 
-const test = time => {
-    return new Promise(resolve => {
-        setTimeout(() => resolve(), time);
-    });
-};
+// for (let i = 0; i < arr.length; i++) {
+//     result += arr[i];
+// }
+// console.log(result);
 
-// test(2000).then(() => console.log('2000ms'));
-// test(3000).then(() => console.log('3000ms'));
+// const arr = ['apple', 'pear', 'plum'];
 
-Promise.all([test(1000), test(2000)]).then(() => {
-    console.log('all');
+// const res = arr.reduce((sum, current) => `${sum}, ${current}`);
+
+// console.log(res);
+
+// const obj = {
+//     ivan: 'persone',
+//     ann: 'persone',
+//     dog: 'animal',
+//     cat: 'animal'
+// };
+
+// const newArr = Object.entries(obj)
+// .filter(item => item[1] == 'persone')
+// .map(item => item[0]);
+
+// console.log(newArr);
+
+
+// let data = [5, 10, 'Shopping', 20, 'Homework'];
+// let result = [];
+// for (let i = 0; i < data.length; i++) {
+//     result[i] = data[(data.length - 1) - i];
+// }
+
+// console.log(result);
+
+
+// console.log(data.reverse());
+
+
+// for (let i = 0; i < data.length; i++) {
+//     if (!isNaN(data[i])) {
+//         data[i] = data[i] * 2;
+        
+//      } else if (typeof(data[i]) === 'string') {
+//         data[i] = `${data[i]} - done`;
+//      }
+// }
+
+
+// ------------------------------
+
+// let newData = [];
+
+// for (let i = 0; i < data.length; i++) {
+//     newData[i] = data[i];
+// }
+ 
+
+// console.log(data);
+// console.log(newData);
+
+let data = [5, 10, 'Shopping', 20, 'Homework'];
+
+const result = data.map((item) => {
+    if (typeof(item) == 'string') {
+        return `${item} - done`;
+    } else if (typeof (item) == 'number') {
+        return item * 2;
+    }
 });
+console.log(data);
+console.log(result);
